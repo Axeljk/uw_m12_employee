310a2abc2722ad71f2fff0b405b9bbbca6fa2205
axeljk <axeljkern@yahoo.com>
2022-07-25 13:16:31 -0400

    Begins creating the skeleton to the program. Init and menu selection are in. Outline for some of the choices are in too.

---
+++ index.js
@@ -0,0 +1,55 @@
+const tables = require("console.table");
+const inquirer = require("inquirer");
+const mySql = require("mysql2");
+const { KEYWORDS, QUESTIONS } = require("./util/promptQuestions");
+const [KEY_VIEW_EMPL, KEY_ADD_EMPL, KEY_EMPL_ROLE, KEY_VIEW_ROLE, KEY_ADD_ROLE, KEY_VIEW_DEPT, KEY_ADD_DEPT, QUIT] = KEYWORDS;
+
+const employees = [];
+const roles = [];
+const departments = [];
+
+
+function init() {
+	QUESTIONS.setVars(employees, roles, departments);
+	selection();
+}
+
+function selection() {
+	inquirer
+	.prompt(QUESTIONS.menu)
+	.then(answers => {
+		switch (answers.selection) {
+			case (KEY_VIEW_EMPL):
+				console.log(KEY_VIEW_EMPL);
+				break;
+			case (KEY_ADD_EMPL):
+				addEmployee();
+				break;
+			case (KEY_EMPL_ROLE):
+				updateEmployee();
+				break;
+			default:  // Quit
+
+		}
+	});
+}
+
+function addEmployee() {
+	inquirer
+	.prompt(QUESTIONS.newEmpl)
+	.then(answers => {
+		console.log("DOING THE ADD EMPLOYEE THING");
+		main();
+	});
+}
+
+function updateEmployee() {
+	inquirer
+	.prompt(QUESTIONS.updateEmpl)
+	.then(answers => {
+		console.log("DOING THE UPDATE EMPLOYEE THING");
+		main();
+	})
+}
+
+init();
