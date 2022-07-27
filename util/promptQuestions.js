7ebaaf8ef25661911e997ceb9b5594cb3388cb7d
axeljk <axeljkern@yahoo.com>
2022-07-27 03:13:11 -0400

    Figured-out how to properly pass queries to inquirer prompts.

--- util/promptQuestions.js
+++ util/promptQuestions.js
@@ -1,13 +1,6 @@
 const KEYWORDS = ["View Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"];
 const [KEY_VIEW_EMPL, KEY_ADD_EMPL, KEY_EMPL_ROLE, KEY_VIEW_ROLE, KEY_ADD_ROLE, KEY_VIEW_DEPT, KEY_ADD_DEPT, QUIT] = KEYWORDS;
-
-let EMPL = [];
-let ROLE = [];
-let DEPT = [];
-
-const listEmpl = () => EMPL.map(e => e.first + e.last);
-const listRole = () => ROLE.map(e => e.name);
-const listDept = () => DEPT.map(e => e.name);
+const { getDepts, getRoles, query } = require("./queryUtil");
 
 const QUESTIONS = {
 	menu: [{
@@ -15,17 +8,17 @@ const QUESTIONS = {
 		choices: () => {
 			let options = [KEY_ADD_DEPT, QUIT];
 
-			if (DEPT.length > 0) {
+//			if (DEPT.length > 0) {
 				options.unshift(KEY_VIEW_DEPT);
 				options.unshift(KEY_ADD_ROLE);
-			}
-			if (ROLE.length > 0)
+//			}
+//			if (ROLE.length > 0)
 				options.unshift(KEY_VIEW_ROLE);
-			if (ROLE.length > 0 && EMPL.length > 0)
+//			if (ROLE.length > 0 && EMPL.length > 0)
 				options.unshift(KEY_EMPL_ROLE);
-			if (DEPT.length > 0 && ROLE.length > 0 && EMPL.length > 0)
+//			if (DEPT.length > 0 && ROLE.length > 0 && EMPL.length > 0)
 				options.unshift(KEY_ADD_EMPL);
-			if (EMPL.length > 0)
+//			if (EMPL.length > 0)
 				options.unshift(KEY_VIEW_EMPL);
 
 			return options;
@@ -45,25 +38,25 @@ const QUESTIONS = {
 	{
 		type: "list",
 		message: "Employee's role:",
-		choices: listRole(),
+		choices: () => query("SELECT title AS name FROM roles"),
 		name: "emplRole"
 	},
 	{
 		type: "list",
 		message: "Employee's manager:",
-		choices: listEmpl(),
+		choices: () => query("SELECT first_name AS name FROM employees"),
 		name: "emplMan"
 	}],
 	updateEmpl: [{
 		type: "list",
 		message: "Update which employee:",
-		choices: listEmpl(),
+		choices: () => query("SELECT first_name AS name FROM employees"),
 		name: "updateName"
 	},
 	{
 		type: "list",
 		message: "Employee's new role:",
-		choices: listRole(),
+		choices: () => query("SELECT title AS name FROM roles"),
 		name: "updateRole"
 	}],
 	newRole: [{
@@ -80,19 +73,14 @@ const QUESTIONS = {
 	{
 		type: "list",
 		message: "Department of role:",
-		choices: listDept(),
+		choices: () => query("SELECT * FROM departments"),
 		name: "roleDept"
 	}],
 	newDept: [{
 		type: "input",
 		message: "Name of department:",
 		name: "deptName"
-	}],
-	setVars: function(employees, roles, departments) {
-		EMPL = employees;
-		ROLE = roles;
-		DEPT = departments;
-	}
+	}]
 };
 
 module.exports = { KEYWORDS, QUESTIONS };
